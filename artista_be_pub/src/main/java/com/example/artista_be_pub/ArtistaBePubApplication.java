cfaf88b746590fc8476dcb56d94f2ded28fabc64
leejooho <tjrwn1003@gmail.com>
2024-05-21 22:27:59 +0900

    최초 푸시

---
+++ artista_be_pub/src/main/java/com/example/artista_be_pub/ArtistaBePubApplication.java
@@ -0,0 +1,23 @@
+package com.example.artista_be_pub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class ArtistaBePubApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ArtistaBePubApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtistaBePubApplication.class, args);
+    }
+
+}
---
+++ artista_be_pub/src/test/java/com/example/artista_be_pub/ArtistaBePubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artista_be_pub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistaBePubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
